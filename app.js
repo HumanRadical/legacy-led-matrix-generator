7aafbe636de47f4489e8914279a36234f34fc502
Easton Meth <easton.meth@gmail.com>
2023-03-21 00:05:29 -0400

    Initialize the program

---
+++ app.js
@@ -0,0 +1,41 @@
+const textbox = document.querySelector("#textbox")
+const form = document.querySelector("form")
+const submit = document.querySelector("#submit")
+const pixelGrid = document.querySelectorAll(".pixel")
+const snake = document.querySelector("#snake")
+
+snakeGrid = (array) => {
+    const newArray = []
+
+    for(let i = 0; i < 16; i++) {
+        const row = []
+
+        for(let j = 0; j < 16; j++) {
+            row.push(array[16 * i + j])
+        }
+
+        if(i % 2 === 1) {
+            row.reverse()
+        }
+
+        newArray.push(row)
+    }
+
+    const finalArray = [].concat(...newArray);
+    return finalArray
+}
+
+addGridColors = () => {
+    let colorInput = JSON.parse(textbox.value)
+
+    if (snake.checked) {
+        colorInput = snakeGrid(colorInput)
+    }
+
+    colorInput.forEach((color, index) => {
+        const pixel = pixelGrid[index]
+        pixel.style.backgroundColor = `#${color}`
+    })
+}
+
+submit.addEventListener("click", addGridColors)
