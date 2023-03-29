1592f2404cd493027bf4b7d5ac2bca8bbd4fc5a3
Easton Meth <easton.meth@gmail.com>
2023-03-29 15:03:35 -0400

    Change cursor type to pointer on draw submit button

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 const inputBox = document.querySelector("#inputBox")
 const submitForm = document.querySelector("#submitForm")
 const grid = document.querySelector("#grid")
-<<<<<<< HEAD:main.js
-<<<<<<< HEAD:app.js
-const snake = document.querySelector("#snakeBox")
-const errorMessage = document.querySelector("#errorMessage")
-=======
 const snakeBox = document.querySelector("#snakeBox")
->>>>>>> draw-mode:main.js
-=======
-const snakeBox = document.querySelector("#snakeBox")
->>>>>>> 0cb3316 (Add colour type option and snake mode option for output):main/app.js
 
-const createGrid = (x, y) => {
+createGrid = (x, y) => {
     grid.innerHTML = ""
 
     for (let i = 0; i < x * y; i++) {
         const pixel = document.createElement("div")
         pixel.style.width = `${400 / x}px`
         pixel.style.height = `${400 / y}px`
+        pixel.style.display = "inline-block"
         pixel.classList.add("pixel")
         grid.append(pixel)
     }
 }
 
-const snakeGrid = (arr, x, y) => {
+snakeGrid = (arr, x, y) => {
     const newArray = []
 
     for(let i = 0; i < y; i++) {
@@ -42,13 +34,8 @@ const snakeGrid = (arr, x, y) => {
     return finalArray
 }
 
-<<<<<<< HEAD:app.js
-const addGridColors = (e) => {
-    e.preventDefault()
-=======
 addGridColors = (event) => {
     event.preventDefault()
->>>>>>> draw-mode:main.js
 
     const x = document.querySelector("#x-axis").value
     const y = document.querySelector("#y-axis").value
@@ -57,37 +44,13 @@ addGridColors = (event) => {
 
     let colorInput = JSON.parse(inputBox.value)
 
-<<<<<<< HEAD:main.js
-<<<<<<< HEAD:app.js
-    errorMessage.innerText = ""
-    if (colorInput.length !== pixels.length) {
-        errorMessage.innerText = "The number of colors does not match the number of pixels."
-    }
-
-    if (snake.checked) {
-=======
     if (snakeBox.checked) {
->>>>>>> draw-mode:main.js
-=======
-    if (snakeBox.checked) {
->>>>>>> 0cb3316 (Add colour type option and snake mode option for output):main/app.js
         colorInput = snakeGrid(colorInput, x, y)
     }
 
     colorInput.forEach((color, index) => {
         const pixel = pixels[index]
-        const targetColor = `#${color}`
-        function isHexCode(color) {
-            return /^#[0-9A-F]{6}$/i.test(color);
-        }
-        if (!isHexCode(targetColor)) {
-            errorMessage.innerText = "One or more pixels has an invalid color."
-            pixel.innerHTML = `
-                <img src="./img/error_icon.svg" alt="error" />
-            `
-        } else {
-            pixel.style.backgroundColor = targetColor
-        }
+        pixel.style.backgroundColor = `#${color}`
     })
 }
 
