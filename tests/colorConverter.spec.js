3339fcedc88b03372e9065a52dd7c57524cd00e0
Easton Meth <easton.meth@gmail.com>
2023-03-29 15:28:29 -0400

    Set up vite.config.js

--- tests/colorConverter.spec.js
+++ tests/colorConverter.spec.js
@@ -1,5 +1,3 @@
-import {describe, test, expect} from "vitest"
-
 describe("Color Converter Test", () => {
     test("If color is a hex code beginning with 0x, change it to a hex string without 0x", () => {
         expect(2 + 2).toBeGreaterThan(1)
