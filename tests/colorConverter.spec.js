14f9e43467a6b326489e68c3944b28ee194a2450
Easton Meth <easton.meth@gmail.com>
2023-03-31 15:20:06 -0400

    Get test passing

--- tests/colorConverter.spec.js
+++ tests/colorConverter.spec.js
@@ -1,5 +1,31 @@
+function sanitizeColourArrayIntoHex(array) {
+    const newArray = array.replaceAll(/0x([\da-f]+)/ig, '"#$1"')
+    const newerArray = JSON.parse(newArray)
+    return newerArray
+}
+
 describe("Color Converter Test", () => {
     test("If color is a hex code beginning with 0x, change it to a hex string without 0x", () => {
-        expect(2 + 2).toBeGreaterThan(1)
+        // Arrange 
+        const rawColourArray = "[0x000000, 0xFF00BB]"
+        const expectedSanitizedArray = ["#000000", "#FF00BB"]
+
+        // Act 
+        const actualSanitizedArray = sanitizeColourArrayIntoHex(rawColourArray);
+
+        // Assert
+        expect(actualSanitizedArray).toEqual(expectedSanitizedArray)
+    })
+
+    test.skip("It accepts predefied prefixes for the sanitized colour", () => {
+        // Arrange 
+        const rawColourArray = "[0x000000, 0xFF00BB]"
+        const expectedSanitizedArray = ["0x000000", "0xFF00BB"]
+
+        // Act 
+        const actualSanitizedArray = sanitizeColourArrayIntoHex(rawColourArray, "0x");
+
+        // Assert
+        expect(actualSanitizedArray).toEqual(expectedSanitizedArray)
     })
 })
