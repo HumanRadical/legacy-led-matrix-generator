a360b73b31419eeef3cc6dae5dff1f6d256e654f
Vehikl Team <go@vehikl.com>
2023-04-06 16:19:38 -0400

    use guard clauses for exception path

--- src/sanitizeColourArrayIntoHex.js
+++ src/sanitizeColourArrayIntoHex.js
@@ -1,13 +1,13 @@
 export function sanitizeColourArrayIntoHex(allColoursInString, prefix = '#') {
-    return allColoursInString
-      .replaceAll(/[ \[\]]/ig, '')
-      .split(',')
-      .map(colour => {
-          const isValidHexColour = colour.match(/^0x[\da-f]+$/ig);
-          if (isValidHexColour) {
-              return colour.replaceAll(/0x([\da-f]+)/ig, `${prefix}$1`);
-          }
+  return allColoursInString
+    .replaceAll(/[ \[\]]/ig, '')
+    .split(',')
+    .map(colour => {
+      const isValidHexColour = colour.match(/^0x[\da-f]+$/ig);
+      if (!isValidHexColour) {
+        return '<Error>';
+      }
 
-          return '<Error>';
-      });
+      return colour.replaceAll(/0x([\da-f]+)/ig, `${prefix}$1`);
+    });
 }
