ae9bb2a064f4502e135c3a56710596adbc5d5e6e
Vehikl Team <go@vehikl.com>
2023-04-06 16:10:43 -0400

    remove clutter

--- tests/colorConverter.spec.js
+++ tests/colorConverter.spec.js
@@ -2,26 +2,20 @@ import { sanitizeColourArrayIntoHex } from "../src/sanitizeColourArrayIntoHex"
 
 describe("Colour Converter Test", () => {
     test("If colour is a hex code beginning with 0x, change it to a hex string without 0x", () => {
-        // Arrange 
         const rawColourArray = "[0x000000, 0xFF00BB]"
         const expectedSanitizedArray = ["#000000", "#FF00BB"]
 
-        // Act 
         const actualSanitizedArray = sanitizeColourArrayIntoHex(rawColourArray);
 
-        // Assert
         expect(actualSanitizedArray).toEqual(expectedSanitizedArray)
     })
 
     test("It accepts predefined prefixes for the sanitized colour", () => {
-        // Arrange 
         const rawColourArray = "[0x000000, 0xFF00BB]"
         const expectedSanitizedArray = ["0x000000", "0xFF00BB"]
 
-        // Act 
         const actualSanitizedArray = sanitizeColourArrayIntoHex(rawColourArray, "0x");
 
-        // Assert
         expect(actualSanitizedArray).toEqual(expectedSanitizedArray)
     })
 
