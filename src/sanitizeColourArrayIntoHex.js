6ff44975ad4dcd519741eee58933f39eebfaf47c
Easton Meth <easton.meth@gmail.com>
2023-04-11 14:44:11 -0400

    Add tab

--- src/sanitizeColourArrayIntoHex.js
+++ src/sanitizeColourArrayIntoHex.js
@@ -1,6 +1,6 @@
 export function sanitizeColourArrayIntoHex(allColoursInString, prefix = '#') {
   return allColoursInString
-  .replaceAll(' ', '')
+    .replaceAll(' ', '')
     .split(',')
     .map(colour => {
       const hexColourRegEx = /0x([\da-f]+)/ig
