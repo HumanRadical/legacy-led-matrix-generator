891079d4578254392c3ed109cfe4b404ff499845
Easton Meth <easton.meth@gmail.com>
2023-04-28 15:01:41 -0400

    Change vite entry point

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
 
 export default defineConfig({
   test: {
     globals: true,
   },
+  rollupInputOptions: {
+    input: resolve(__dirname, 'views/index.html')
+  }
 })
