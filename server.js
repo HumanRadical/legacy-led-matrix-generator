f708f17fd582de2918c366d31aa5315684f4a4c4
Easton Meth <easton.meth@gmail.com>
2023-05-05 14:56:12 -0400

    Insert vite-plugin-ssr code

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const path = require('path')
-// const ViteExpress = require('vite-express');
+const compression = require('compression')
+const port = process.env.PORT || 5173
+
+const root = `${__dirname}/views`
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'))
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+startServer()
+
+async function startServer() {
+  const app = express()
+
+  app.use(compression())
+
+  const vite = require('vite')
+  const viteDevMiddleware = (
+      await vite.createServer({
+      root,
+      server: { middlewareMode: true }
+      })
+  ).middlewares
+  app.use(viteDevMiddleware)
+
+  app.listen(port)
+  console.log(`Server running at http://localhost:${port}`)
+}
 
 app.get('/', (req, res) => {
     res.redirect('/main')
@@ -18,9 +41,5 @@ app.get('/main', (req, res) => {
 
 app.get('/draw', (req, res) => {
     res.render('draw')
-})
-
-app.listen(5173, () => {
-    console.log('Listening on port 5173')
 })
 // ViteExpress.listen(app, 5173, () => console.log("Server is listening..."));
